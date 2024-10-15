99b63e3dcce031424316feb681eeee9175736695
drdata1010 <159292707+drdata1010@users.noreply.github.com>
2024-10-16 00:30:27 +0530

    Testing return 0 with failed logic

---
+++ src/main/java/com/learning/utils/Calculator.java
@@ -0,0 +1,7 @@
+package com.learning.utils;
+
+public class Calculator {
+    public static int add(String input){
+        return -2;
+    }
+}
---
+++ src/test/java/com/learning/utils/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.learning.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static com.learning.utils.Calculator.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void checkEmptyReturn0(){
+        assertEquals(0, add(""));
+    }
+}
